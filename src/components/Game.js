28985fe7378adc3f6ee618e827aae932f2adff90
dpisonero <davidpisonero96@gmail.com>
2018-12-27 21:51:31 +0100

    Navbar

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Content from "./Content";
 import Button from "./Button"
+import Navbar from "./Navbar";
 
 export default class Game extends React.Component {
 
@@ -11,11 +12,15 @@ export default class Game extends React.Component {
 
         if (this.props.question === undefined) {
             return(
-                <h1>No hay preguntas en el servidor</h1>
+                <div>
+                    <Navbar/>
+                    <h1>No hay preguntas en el servidor</h1>
+                </div>
             )
         } else {
             return (
                 <div>
+                    <Navbar/>
                     <h3>Su puntuación es: {this.props.score} ({this.props.score * 100 / this.props.questions.length} % de aciertos)</h3>
 
                     <Content question={this.props.question} onQuestionAnswer={this.props.onQuestionAnswer}/>
